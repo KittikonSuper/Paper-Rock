ba3ee8e1263749774621e367a83fed1ccece5de1
kittikon <pagsky_2499@hotmail.com>
2016-01-19 19:47:14 -0800

    ฉันได้ทำการ widget แล้ววว

--- app/src/main/java/com/kru/pag/gamepaperrockscissors/MainActivity.java
+++ app/src/main/java/com/kru/pag/gamepaperrockscissors/MainActivity.java
@@ -2,12 +2,36 @@ package com.kru.pag.gamepaperrockscissors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Explicit
+    private ImageView paperImageView, rockImageView,
+            scissorImageView, playImageViwe, androidImageView;
+    private TextView showtextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bind widget
+        bindwidget();
+    }// Main Metthod
+
+    private void bindwidget() {
+        paperImageView = (ImageView) findViewById(R.id.imvPaper);
+        rockImageView = (ImageView) findViewById(R.id.imvRock);
+        scissorImageView = (ImageView) findViewById(R.id.imvScissors);
+        playImageViwe = (ImageView) findViewById(R.id.imvPlayer);
+        androidImageView = (ImageView) findViewById(R.id.imvAndroid);
+        showtextView = (TextView) findViewById(R.id.txtShow);
+
+
     }
-}
+} // Main Class นี่คือคลาสหลัก
